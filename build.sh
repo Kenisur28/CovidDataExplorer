a201a3bc1418686bb4ec55a12d7418e6c4580a2a
Kenisur28 <75283680+Kenisur28@users.noreply.github.com>
2022-02-06 19:33:31 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 echo Running Docker
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
- sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io
- 
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+
+
 docker run . covid-data-explorer 

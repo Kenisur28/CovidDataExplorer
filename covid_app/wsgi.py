1959a74b1c1431966b21ec27f48637becb8d6643
Kenisur28 <75283680+Kenisur28@users.noreply.github.com>
2022-02-06 21:15:43 -0500

    Update wsgi.py

--- covid_app/wsgi.py
+++ covid_app/wsgi.py
@@ -2,8 +2,6 @@
 from covidApp import my_app as app
 
 
-
-if __name__ == "__main__":
-    application = app.server
-    application.run(host="127.0.0.1", port=8080)
+application = app.server
+application.run(host="127.0.0.1", port=8080)
     

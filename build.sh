4695c90a22212ff61dbe5291e54b868b62e960fe
Daniel Rusinek <drusinek28@gmail.com>
2022-02-08 17:18:41 -0500

    shrunk layout

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-echo Running Docker
-
-chmod +x /covid_app/wsgi.py
-./covid_app/wsgi.py
--- covid_app/wsgi.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from covidApp import my_app as app
-
-
-application = app.server
-application.run(host="127.0.0.1", port=8080)
-    
